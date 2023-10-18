caa1f6ad93773c3428909ef25a8ee2fd6c9ae73d
Taha <tahalammouchi005@gmail.com>
2023-10-18 01:52:02 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,50 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchThreads } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+import Thread from "@/lib/models/thread.model";
+import { UserButton, currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+ 
+export default async function Home() {
+  
+  // redirect to onboarding if user is not onboarded
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) {
+    redirect("/onboarding");
+  }
+
+  const result = await fetchThreads(1,10);
+
+
+  return (
+   <>
+      <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.threads.length === 0 ? (
+          <p className="no-result"> No threads found</p>
+          ) : (
+            <>
+            {result.threads.map((thread) => (
+              <ThreadCard 
+                key={thread._id}
+                id={thread._id}
+                currentUserId={user?.id}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+               />
+            ))}
+            </>
+          )
+        }
+
+      </section>
+   </>
+  )
+}
---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,141 @@
+import { metadata } from './../../app/(root)/layout';
+"use server"
+
+import { revalidatePath } from "next/cache";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose"
+
+interface Params {
+    text: string,
+    author: string,
+    communityId: string | null,
+    path: string
+}
+
+
+// create thread
+export async function createThread({ text, author, communityId, path }: Params) {
+    try {
+        connectToDB();
+        const createdThread = await Thread.create({
+            text,
+            author,
+            community: null,
+        });
+
+        //Update user model
+        const user = await User.findById(author);
+        user.threads.push(createdThread._id);
+        await user.save();
+
+        //revalidate path
+        revalidatePath(path);
+    } catch (error: any) {
+        throw new Error(`Error creating thread: ${error.message}`);
+    }
+}
+
+// fetch threads that have no parents (top level threads)
+export async function fetchThreads(pageNumber = 1, pageSize = 20){
+    try {
+        connectToDB();
+
+        // calculate the number of threads to skip
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const threads = await Thread.find({ parentId: {$in: [null, undefined]} })
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({path: 'author', model: 'User'})
+        .populate({
+            path: 'children',
+            model: 'Thread',
+            populate: {
+                path: 'author',
+                model: 'User',
+                select: "_id name parentId image"
+            }
+        }).exec();
+
+        const totalThreadsCount = await Thread.countDocuments({ parentId: {$in: [null, undefined]} });
+
+        const isNext  = totalThreadsCount > skipAmount + threads.length;
+
+        return { threads, isNext };
+    } catch (error: any) {
+        throw new Error(`Error fetching threads: ${error.message}`);
+    }
+}
+
+// fetch thread by id
+export async function fetchThreadById(id: string) {
+    try {
+        connectToDB();
+        const thread = await Thread.findById(id)
+        .populate({path: 'author', model: 'User', select: "_id id name image"})
+        .populate({
+            path: 'children',
+            model: 'Thread',
+            populate: [
+            {
+                path: 'author',
+                model: 'User',
+                select: "_id id name parentId image"
+            },
+            {
+                path: 'children',
+                model: 'Thread',
+                populate: {
+                path: 'author',
+                model: 'User',
+                select: "_id id name parentId image"
+                }
+            }
+          ]
+        }).exec();
+        return thread;
+    } catch (error: any) {
+        throw new Error(`Error fetching thread: ${error.message}`);
+    }  
+}
+
+// add comment to thread
+export async function addCommentToThread({ threadId, commentText, userId, pathname }:  {
+    threadId: string,
+    commentText: string,
+    userId: string,
+    pathname: string
+}) {
+    connectToDB();
+    try {
+
+      // Find the original thread by its ID
+      const originalThread = await Thread.findById(threadId);
+  
+      if (!originalThread) {
+        throw new Error("Thread not found");
+      }
+      // Create the new comment thread
+      const commentThread = new Thread({
+        text: commentText,
+        author: userId,
+        parentId: threadId, // Set the parentId to the original thread's ID
+      });
+      // Save the comment thread to the database
+      const savedCommentThread = await commentThread.save();
+      // Add the comment thread's ID to the original thread's children array
+      originalThread.children.push(savedCommentThread._id);
+      // Save the updated original thread to the database
+      await originalThread.save();
+      revalidatePath(pathname);
+      
+    } catch (err) {
+      console.error("Error while adding comment:", err);
+      throw new Error("Unable to add comment");
+    }
+  }
+
+
+
