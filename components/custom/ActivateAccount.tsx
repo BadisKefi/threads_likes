aa67d596cb2679c769955b0b75a052f6639ab9f4
Taha <tahalammouchi005@gmail.com>
2023-11-02 01:45:11 +0000

    sprint 1

--- components/custom/ActivateAccount.tsx
+++ components/custom/ActivateAccount.tsx
@@ -9,6 +9,9 @@ import { redirect, useRouter } from "next/navigation";
 export default function ActivateAccount({ userInfo } : { userInfo: any }) {
     const toast = useToast();
     const router = useRouter();
+    if(!userInfo.id) {
+        return null;
+    }
     if(userInfo.status === 'active') {
         redirect('/');
     }
